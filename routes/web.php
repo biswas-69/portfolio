7240138c90628004b69ea9ef336737b354443aed
biswas parajuli <biswasparajuli009@gmail.com>
2025-06-16 10:34:59 +0545

    new update

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+Route::get('/index', function () {
+    return view('index');
+});
+
+use App\Http\Controller\IndexController;
+
+Route::get('/home',[IndexController::class,'index']);
