7a46e854baa272627c15e4e3bcb201267654aa31
biswas parajuli <biswasparajuli009@gmail.com>
2025-06-18 09:28:36 +0545

    new update cv

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -12,12 +12,9 @@
     </style>
 </head>
 <body>
-    <h1>Photo Uploaded Successfully!</h1>
+    
 
     <img src="{{ asset('storage/' . $photo->file_path) }}" alt="Uploaded Photo" width="300"><br><br>
-    <p><strong>File Name:</strong> {{ $photo->name }}</p>
-
-    <a href="{{ route('photos.download', $photo->id) }}">Download This Photo</a><br><br>
-    <a href="{{ route('photos.upload') }}">Upload Another Photo</a>
+    
 </body>
 </html>
