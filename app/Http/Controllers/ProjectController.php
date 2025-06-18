7f9c2069a308f1e57d46d9454f303be7b82f7ed7
anju <anjupurkuti6@gmail.com>
2025-06-18 21:56:03 +0300

    Backend of project file

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ProjectRequest;
 use App\Models\project;
 
+
 class ProjectController extends Controller
 {
     public function project()
     {
         $projects = Project::latest()->get();
-        return view('project', compact('projects'));
+        return view('project', ['projects' => $projects]);
     }
      public function add()
      {
@@ -27,5 +28,23 @@ class ProjectController extends Controller
         }
         Project::create($validated);
         return redirect('/');
-     }
+     } 
+     public function update(ProjectRequest $request)
+    {  
+        $validated = $request->validated();
+        if($request->hasFile('photo')){
+            $path =  $request->file('photo')->store('images','local');
+            $validated['photo'] = 'storage/' . $path;
+        }
+
+        Project::where('id', $request['id'])->update($validated);
+
+        return redirect('/');
+    }
+
+    public function updateView($id)
+    {
+        $pro = Project::where('id', $id)->get();
+        return view('update', ['pro'=>$pro[0]]);
+    }
 }
